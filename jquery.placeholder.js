35c8de30f76e2d9c7cc63c385fa05f28a66d8878
Monk <stephanzych@gmail.com>
2012-04-11 18:16:41 +0300

    --

--- jquery.placeholder.js
+++ jquery.placeholder.js
@@ -1,22 +1,34 @@
 (function($){
 	$.fn.placeholder = function(){
+		
 		return this.each(function(){
 			if(!!('placeholder' in document.createElement('input')))	return;
 			
-			var field = $(this), value = field.attr('placeholder');
+			$(this)
+			.submit(function(){
+				$(this).find('[placeholder]')
+				.each(function(){
+					if($(this).val() == $(this).attr('placeholder'))	$(this).val('');	
+				});
+			})
+			.find('[placeholder]')
+			.each(function(){
+				var field = $(this), value = field.attr('placeholder');
 					
-			field
-			.bind('focusin', function(){
-				if($(this).val() == value)	$(this).removeClass('placeholder').val('');
-			})
-			.bind('focusout', function(){
-				if($(this).val() == '')	$(this).addClass('placeholder').val(value);
-			})
-			.val(value).addClass('placeholder');
+				field
+				.bind('focusin', function(){
+					if($(this).val() == value)	$(this).removeClass('placeholder').val('');
+				})
+				.bind('focusout', function(){
+					if($(this).val() == '')	$(this).addClass('placeholder').val(value);
+				})
+				.val(value).addClass('placeholder');
+			});
+			
 		});
 	};
 	$(function(){
-		$('[placeholder]').placeholder();
+		$('form').placeholder();
 	});
 	// ex.: <input id="email_input" name="email" type="email" value="" placeholder="your@mail.tld" />
 })(jQuery); // jQuery.placeholder() by Stéphan Zych (monkeymonk.be)
