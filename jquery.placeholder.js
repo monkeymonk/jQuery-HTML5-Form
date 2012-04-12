a2597592fdc797bbc88dce6400d3d68fcf541fb4
Monk <stephanzych@gmail.com>
2012-04-12 11:19:53 +0300

    input type password is now supported /!\ submit must be reviewed

--- jquery.placeholder.js
+++ jquery.placeholder.js
@@ -1,34 +1,52 @@
 (function($){
-	$.fn.placeholder = function(){
+	$.fn.placeholder = function(options){
+		var defaults = {className: 'placeholder'}, s = $.extend({}, defaults, options);
 		
 		return this.each(function(){
-			if(!!('placeholder' in document.createElement('input')))	return;
+			if('placeholder' in document.createElement('input'))	return;
 			
-			$(this)
-			.submit(function(){
-				$(this).find('[placeholder]')
-				.each(function(){
-					if($(this).val() == $(this).attr('placeholder'))	$(this).val('');	
-				});
-			})
-			.find('[placeholder]')
-			.each(function(){
-				var field = $(this), value = field.attr('placeholder');
-					
-				field
-				.bind('focusin', function(){
-					if($(this).val() == value)	$(this).removeClass('placeholder').val('');
+			var o = $(this), value = o.attr('placeholder');
+			
+			if(this.type == 'password'){
+				var l = o.offset().left, t = o.offset().top;
+				
+				o.bind('focusin', function(){
+					$(this).next('span.' + s.className).hide();
 				})
 				.bind('focusout', function(){
-					if($(this).val() == '')	$(this).addClass('placeholder').val(value);
+					if($(this).val() == '')	$(this).next('span.' + s.className).show();
 				})
-				.val(value).addClass('placeholder');
-			});
+				.after('<span class="' + s.className + '">' + value + '</span>')
+				.next('span.' + s.className)
+				.css({left: l, position: 'absolute', top: t})
+				.bind('click', function(){o.focus();});
+			} else {
+				o.bind('focusin', function(){
+					if($(this).val() == value)	$(this).removeClass(s.className).val('');
+				})
+				.bind('focusout', function(){
+					if($(this).val() == '')	$(this).addClass(s.className).val(value);
+				})
+				.val(value).addClass(s.className);
+			}
 			
+			o.submit(function(){
+				var submit = true;
+				
+				$(this).find('[placeholder]')
+				.each(function(){
+					if($(this).val() == $(this).attr('placeholder')){
+						$(this).val('');
+						submit = false;
+					}
+				});
+				
+				return submit;
+			});
 		});
 	};
+	
 	$(function(){
-		$('form').placeholder();
+		$('[placeholder]').placeholder();
 	});
-	// ex.: <input id="email_input" name="email" type="email" value="" placeholder="your@mail.tld" />
 })(jQuery); // jQuery.placeholder() by Stéphan Zych (monkeymonk.be)
