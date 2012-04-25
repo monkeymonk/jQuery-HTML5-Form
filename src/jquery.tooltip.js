7fe663a8cc95fbbe7df39688de200346f9f6f589
Zych Stéphan <stephanzych@gmail.com>
2012-04-25 17:49:52 +0200

    --

--- src/jquery.tooltip.js
+++ src/jquery.tooltip.js
@@ -2,7 +2,7 @@
 	$.fn.tooltip = function(method){
 		var defaults = {
 			idName: 'tooltip'
-			, text: ''
+			, tip: ''
 			, onShow: function(){}
 			, onHide: function(){}
 		}, s = {};
@@ -12,27 +12,25 @@
 				s = $.extend({}, defaults, options);
 				
 				return this.each(function(){
-					var o = $(this), tooltip = $('<div id="' + s.idName + '"></div>');
+					var o = $(this);
 					
 					methods.destroy.call(this);
 					
-					o.bind('mouseenter.tooltip', function(){
-						s.text = $(this).attr('title') || s.text;
+					$('body').append('<div id="' + s.idName + '" style="left:-999em;opacity:0;"/>');
+					
+					o.attr('data-tooltip-title', o.attr('title'))
+					.bind('mouseenter.tooltip', function(){
+						var tip = $(this).attr('title') || s.tip;
 						
-						if(!s.text || s.text == '')	return;
+						if(!tip || tip == '')	return;
 						
-						$(this).removeAttr('title');
-						
-						if(!$('#' + s.idName).length)	tooltip.appendTo('body');
-						$('#' + s.idName).css({opacity: 0}).html(s.text);
-						
-						methods.show.call(this);
+						methods.show.call(this, tip);
 					})
 					.bind('mouseleave.tooltip', function(){
 						methods.hide.call(this);
 					});
 					
-					tooltip.bind('click.tooltip', function(){
+					$('#' + s.idName).bind('click.tooltip', function(){
 						methods.hide.call(this);
 					});
 					
@@ -43,16 +41,20 @@
 			}, // init
 			
 			destroy: function(){
-				$(this).unbind('.tooltip');
-				$(window).unbind('.tooltip');
-				if($('div#' + s.idName).length)	$('div#' + s.idName).unbind('.tooltip').remove();
+				$(this).unbind('.tooltip').removeAttr('data-tooltip-title');
 			}, // destroy
 			
-			show: function(){
-				var o = $(this), tooltip = $('#' + s.idName), wW = $(window).width(),
-					l = o.offset().left + (o.outerWidth() / 2) - (tooltip.outerWidth() / 2), t = o.offset().top - tooltip.outerHeight() - 20;
+			show: function(){debug('show');
+				if(!$(this).attr('data-tooltip-title'))	methods.init.apply(this, arguments);
 				
-				if(wW < tooltip.outerWidth() * 1.5)	tooltip.css({maxWidth: wW / 2});
+				var o = $(this), tip = arguments[0] || o.attr('title'), tooltip = $('#' + s.idName);
+				
+				o.attr('data-tooltip-title', tip).removeAttr('title');
+				tooltip.html(tip);
+				
+				var win = $(window).width(), l = o.offset().left + (o.outerWidth() / 2) - (tooltip.outerWidth() / 2), t = o.offset().top - tooltip.outerHeight() - 20;
+				
+				if(win < tooltip.outerWidth() * 1.5)	tooltip.css({maxWidth: win / 2});
 				else	tooltip.css({maxWidth: 340});
 				
 				if(l < 0){
@@ -60,7 +62,7 @@
 					tooltip.addClass('left');
 				} else	tooltip.removeClass('left');
 				
-				if(l + tooltip.outerWidth() > wW){
+				if(l + tooltip.outerWidth() > win){
 					l = o.offset().left - tooltip.outerWidth() + o.outerWidth() / 2 + 20;
 					tooltip.addClass('right');
 				} else	tooltip.removeClass('right');
@@ -74,14 +76,13 @@
 				.animate({top: '+=10', opacity: 1}, 50);
 			}, // show
 			
-			hide: function(){
-				var o = $(this), tooltip = $('#' + s.idName);
+			hide: function(){debug('hide');
+				var o = $(this), tip = o.attr('data-tooltip-title'), tooltip = $('#' + s.idName);
 				
 				tooltip.animate({top: '-=10', opacity: 0}, 50, function(){
-					$(this).remove();
+					$(this).css({left: '-999em'});
+					o.attr('title', tip);
 				});
-				
-				o.attr('title', s.text);
 			} // hide
 		}; // methods
 		
