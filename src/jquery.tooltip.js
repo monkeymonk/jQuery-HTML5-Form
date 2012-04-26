3fed709a9f7ef4f10b6d3c794265e88f660f40f1
Zych Stéphan <stephanzych@gmail.com>
2012-04-26 12:08:02 +0200

    tooltip

--- src/jquery.tooltip.js
+++ src/jquery.tooltip.js
@@ -2,7 +2,9 @@
 	$.fn.tooltip = function(method){
 		var defaults = {
 			idName: 'tooltip'
-			, tip: ''
+			, tip: null
+			, position: null
+			, offset: 15
 			, onShow: function(){}
 			, onHide: function(){}
 		}, s = {};
@@ -16,18 +18,19 @@
 					
 					methods.destroy.call(this);
 					
-					$('body').append('<div id="' + s.idName + '" style="left:-999em;opacity:0;"/>');
+					if(!$('#' + s.idName).length)	$('body').append('<div id="' + s.idName + '" style="left:-999em;opacity:0;"/>');
 					
-					o.attr('data-tooltip-title', o.attr('title'))
-					.bind('mouseenter.tooltip', function(){
-						var tip = $(this).attr('title') || s.tip;
-						
-						if(!tip || tip == '')	return;
-						
-						methods.show.call(this, tip);
+					o.attr({
+						'data-tooltip-title': o.attr('title')
+						, 'data-tooltip-position': o.attr('data-tooltip-position') || s.position
 					})
-					.bind('mouseleave.tooltip', function(){
-						methods.hide.call(this);
+					.bind({
+						'mouseenter.tooltip': function(){
+							methods.show.call(this);
+						}
+						, 'mouseleave.tooltip': function(){
+							methods.hide.call(this);
+						}
 					});
 					
 					$('#' + s.idName).bind('click.tooltip', function(){
@@ -44,46 +47,68 @@
 				$(this).unbind('.tooltip').removeAttr('data-tooltip-title');
 			}, // destroy
 			
-			show: function(){debug('show');
+			show: function(){
 				if(!$(this).attr('data-tooltip-title'))	methods.init.apply(this, arguments);
 				
-				var o = $(this), tip = arguments[0] || o.attr('title'), tooltip = $('#' + s.idName);
+				var o = $(this), tip = arguments[0] || o.attr('title'), tooltip = $('#' + s.idName), pos;
+				
+				if(!tip || tip == '')	return;
 				
 				o.attr('data-tooltip-title', tip).removeAttr('title');
 				tooltip.html(tip);
 				
-				var win = $(window).width(), l = o.offset().left + (o.outerWidth() / 2) - (tooltip.outerWidth() / 2), t = o.offset().top - tooltip.outerHeight() - 20;
+				pos = methods.place.call(this);
 				
-				if(win < tooltip.outerWidth() * 1.5)	tooltip.css({maxWidth: win / 2});
-				else	tooltip.css({maxWidth: 340});
-				
-				if(l < 0){
-					l = o.offset().left + o.outerWidth() / 2 - 20;
-					tooltip.addClass('left');
-				} else	tooltip.removeClass('left');
-				
-				if(l + tooltip.outerWidth() > win){
-					l = o.offset().left - tooltip.outerWidth() + o.outerWidth() / 2 + 20;
-					tooltip.addClass('right');
-				} else	tooltip.removeClass('right');
-				
-				if(t < 0){
-					t = o.offset().top + o.outerHeight();
-					tooltip.addClass('top');
-				} else	tooltip.removeClass('tip');
-				
-				tooltip.css({left: l, top: t})
-				.animate({top: '+=10', opacity: 1}, 50);
+				tooltip.animate({pos: '+=10', opacity: 1}, 50);
 			}, // show
 			
-			hide: function(){debug('hide');
+			hide: function(){
 				var o = $(this), tip = o.attr('data-tooltip-title'), tooltip = $('#' + s.idName);
 				
 				tooltip.animate({top: '-=10', opacity: 0}, 50, function(){
 					$(this).css({left: '-999em'});
 					o.attr('title', tip);
 				});
-			} // hide
+			}, // hide
+			
+			place: function(){
+				var o = $(this), position = arguments[0] || o.attr('data-tooltip-position'), tooltip = $('#' + s.idName)
+				, win = $(window).width(), l = o.offset().left + (o.outerWidth() / 2) - (tooltip.outerWidth() / 2), t = o.offset().top - tooltip.outerHeight() - 20;
+				
+				if(!position){
+					if(l < 0)	position = 'left';
+					else if(l + tooltip.outerWidth() > win)	position = 'right';
+					else if(t < 0)	position = 'top';
+					else	position = 'bottom';
+				}
+				
+				tooltip.removeClass('left top right bottom');
+				
+				switch(position){
+					case 'left':
+						l = o.offset().left - tooltip.outerWidth() - s.offset;
+						t = o.offset().top + (o.outerHeight() / 2) - (tooltip.outerHeight() / 2);
+						tooltip.addClass('left');
+						break;
+					case 'top':
+						t = o.offset().top + o.outerHeight() + s.offset;
+						tooltip.addClass('top');
+						break;
+					case 'right':
+						l = o.offset().left + o.outerWidth() + s.offset;
+						t = o.offset().top + (o.outerHeight() / 2) - (tooltip.outerHeight() / 2);
+						tooltip.addClass('right');
+						break;
+					case 'bottom':
+						tooltip.addClass('bottom');
+					default:
+						break;
+				}
+				
+				tooltip.css({left: l, top: t});
+				
+				return position;
+			} // place
 		}; // methods
 		
 		if(methods[method])	return methods[method].apply(this, Array.prototype.slice.call(arguments, 1)); // call the respective method
