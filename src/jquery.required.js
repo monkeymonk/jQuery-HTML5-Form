10317cd3b0b05abfba589af2a0a02b0e2b3c5602
Stéphan Zych <stephanzych@gmail.com>
2012-11-07 19:50:36 +0100

    Rewrite jquery.required.js
    No more option "message", if you need a message use attribute data ! ;-)

--- src/jquery.required.js
+++ src/jquery.required.js
@@ -1,73 +1,100 @@
-(function($){
-	$.fn.required = function(method){
-		var defaults = {
-			className: 'required'
-			, msg: 'Required field' // custom message to display (via callback), can be set by 'data-required' attribute
-			, override: true // force browser to use plugin instead of HTML5 core
-			, onBlur: function(settings, value, isFilled){}
-			, onSubmit: function(settings, isFilled){} // return false to prevent submit
-		}, s = {};
+
+(function($) {
+	"use strict";
+	
+	var defaults = {
+		className: 'required',
+		override: true,
 		
-		var methods = {
-			init: function(options){
-				s = $.extend({}, defaults, options);
-				
-				if(!s.override && 'required' in document.createElement('input'))	return;
-				
-				return this.each(function(){
-					var o = $(this), form = o.closest('form');
-					
-					methods.destroy.call(this);
-					
-					if(s.override)	o.unbind('invalid').bind('invalid.required', function(e){e.preventDefault();});
-					s.msg = o.attr('data-required') || s.msg;
-					
-					if(o.is('[required]:not(:checkbox, :radio)')){
-						o.bind('blur.required', function(){
-							var value = $(this).val();
-							if(value == '' || value == $(this).attr('placeholder')){
-								$(this).addClass(s.className);
-								s.onBlur.call(this, s, value, false);
-							} else {
-								$(this).removeClass(s.className);
-								s.onBlur.call(this, s, value, true);
-							}
-						});
-					} else {
-						o.bind('change.required', function(){
-							var value = $(this).val(), id = $(this).attr('id');
-							if($(this).attr('checked') != 'checked'){
-								$(this).add('label[for="' + id + '"]').addClass(s.className);
-								s.onBlur.call(this, s, value, false);
-							} else {
-								$(this).add('label[for="' + id + '"]').removeClass(s.className);
-								s.onBlur.call(this, s, value, true);
-							}
-						});
-					}
-					
-					form.attr('novalidate', 'novalidate')
-					.bind('submit.required', function(){
-						$(this).find('[required]')
-						.each(function(){
-							$(this).trigger('blur');
-						});
-						return s.onSubmit.call(this, s, !$(this).find('.required').length);
-					});
-				});
-			}, // init
+		// callback
+		onValidate: function(){},
+		onSubmit: function() {}
+	}, settings = {};
+	
+	
+	var methods = {
+		init: function( options ) {
+			settings = $.extend({}, defaults, options);
 			
-			destroy: function(){
-				return $(this).each(function(){$(this).unbind('.required');});
-			} // destroy
-		}; // required
+			if(!settings.override && 'required' in document.createElement('input')) {
+				return;
+			}
+			
+			return this.each(function() {
+				var that = this, $that = $(that), form = $that.closest('form');
+				
+				methods.destroy.call(this);
+				
+				settings.message = that.getAttribute('data-required') || settings.message;
+				
+				if(settings.override) {
+					$that.on('invalid.required', function(e) {
+						e.preventDefault();
+					});
+				}
+				
+				$that
+				.on('keydown.required blur.required', function() {
+					methods.validate.call(this, settings.onValidate);
+				});
+				
+				form.attr('novalidate', 'novalidate')
+				.unbind('submit.required')
+				.on('submit.required', function() {
+					var fields = $(this).find('[required]');
+					
+					fields.each(function() {
+						methods.validate.call(this, settings.onValidate);
+					});
+					
+					return settings.onSubmit.call(this, settings, !fields.is('.required').length);
+				});
+			});
+		}, // init
 		
-		if(methods[method])	return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-		else if(typeof method === 'object' || !method)	return methods.init.apply(this, arguments);
-		else	$.error('Method "' + method + '" does not exist in Required plugin!');
+		validate: function( callback ) {
+			var that = this, value = that.value, label = $(that).add('[for="' + that.id + '"]'), valid = false;
+			
+			callback = callback || settings.onValidate;
+			
+			// if :radio or :checkbox
+			if(this.type.match(/radio|checkbox/i)) {
+				if(that.checked) {
+					valid = true;
+				}
+			} else {
+				if(value && value !== that.placeholder) {
+					valid = true;
+				}
+			}
+			
+			if(valid) {
+				label.removeClass( settings.className );
+			} else {
+				label.addClass( settings.className );
+			}
+			
+			callback.call(this, settings, valid);
+		}, // validate
+		
+		destroy: function() {
+			return $(this).each(function() {
+				$(this).unbind('.required');
+			});
+		} // destroy
 	};
 	
-	$(function(){
-		$('[required]').required();
-	});
-})(jQuery); // jQuery.required() by Stéphan Zych (monkeymonk.be)
+	
+	$.fn.required = function( options ) {
+		
+		if(methods[ options ]) {
+			return methods[ options ].apply(this, Array.prototype.slice.call(arguments, 1));
+		} else if(typeof options === 'object' || !options) {
+			return methods.init.apply(this, arguments);
+		} else {
+			$.error('Method "' + options + '" does not exist in $.required plugin!');
+		}
+		
+	}; // $.fn.required
+	
+})(jQuery);
